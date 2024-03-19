6d596207da8c366633a447d78793b257528b858e
Covald <p.rezin@g.nsu.ru>
2024-03-20 01:26:37 +0700

    Add function in tests for generating matrix by vector.

--- tests/Matrix_test.cpp
+++ tests/Matrix_test.cpp
@@ -3,6 +3,18 @@
 #include "stdexcept"
 #include <iostream>
 
+Matrix &set_matrix_by_vector(Matrix &mtx, const double *array, size_t size) {
+    if (size != mtx.rows() * mtx.cols()) return mtx;
+
+    for (size_t row = 0; row < mtx.rows(); row++) {
+        for (size_t col = 0; col < mtx.cols(); col++) {
+            mtx.coeffRef(row, col) = array[row * mtx.cols() + col];
+        }
+    }
+
+    return mtx;
+}
+
 TEST(Contructors, base_contructor) {
     Matrix m;
     ASSERT_EQ(m.isValid(), false);
